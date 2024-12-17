e5a7420c72c69a643be40c5f08c6ea15b03aa461
OrsonOzie <orsonozie@gmail.com>
2024-12-17 16:10:21 +0300

    Add files via upload

---
+++ Jovieh.py
@@ -0,0 +1,12 @@
+print("welcome to python programming")
+print("Happy coding!!")
+if 5>2:
+    print("Five is greater than two!")
+x=5
+print(x)  
+y="hello, world"
+print(y) 
+x=4
+print(x)#printing
+x= "sally"
+print(x)
